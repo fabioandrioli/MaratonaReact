eb7e5677985067decf14015d3f70e52ade608582
Fabio <fabio.drioli@gmail.com>
2021-08-19 14:14:26 -0300

    Criação da tela signin

---
+++ src/screens/signIn/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {View,Text,Image} from 'react-native'
+import {styles} from './styles'
+
+//images imports
+import IlustrationImg from '../../assets/illustration.png'
+
+//componets imports
+import {ButtonIcon} from '../../components/ButtonIcon/index'
+
+export function SignIn(){
+    return(
+        <View style={styles.container}>
+          
+            <Image 
+                source={IlustrationImg} 
+                resizeMode="stretch"
+                style={styles.image}/>
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas  {`\n`}
+                    facilmente {`\n`}
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie gurpos para jogar seus games{`\n`}
+                    favoritos com seus amigos
+                </Text>
+                <ButtonIcon title="Entre com o Discord"/>
+            </View>
+        </View>
+        
+    )
+}
---
+++ src/screens/signIn/styles.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import {theme} from '../../global/styles/theme'
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor:theme.colors.secondary90,
+    },
+    image:{
+        width:'100%',
+        height:360,
+    },
+    content:{
+        marginTop: -40,
+        paddingHorizontal:50,
+    },
+    title:{
+        color: theme.colors.heading,
+        alignItems:'center',
+        fontSize: 40,
+        marginBottom:16,
+    },
+    subtitle:{
+        color: theme.colors.heading,
+        fontSize:15,
+        textAlign:'center',
+    }
+  });
+  
