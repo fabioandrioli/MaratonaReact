6c53d77c28afb500bd190a6eea32dda5175e5951
Fabio <fabio.drioli@gmail.com>
2021-08-19 15:29:25 -0300

    Configuração do google fontes, usando o hoohe useFonts, instalação da lib expo-app-loading para segurar a tela de splah enquanto as fontes são carregadas e instaladas

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import {StatusBar} from 'react-native'
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import {SignIn} from './src/screens/signIn'
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  
   return (
     <>
         <StatusBar
