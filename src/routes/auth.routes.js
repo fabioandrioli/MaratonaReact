4c9060f3a16f0b19a5704af2fffa70fa1780befe
Fabio <fabio.drioli@gmail.com>
2021-08-20 15:47:55 -0300

    Insatalação do react navigation 5

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,21 +1,20 @@
 import  React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { createStackNavigator } from '@react-navigation/stack';
 import {Home} from '../screens/home'
 import {SignIn} from '../screens/signin'
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 const ModalOptions = {
-    headerShown: false,
     cardStyle: {
         backgroundColor: 'transparent',
       },
-      transparentCard: true,
 }
 
 export function AuthRoutes() {
     return (
         <Stack.Navigator
+            headerMode="none"
             screenOptions={ModalOptions}
             
         >
